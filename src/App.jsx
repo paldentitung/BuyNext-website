22ed45353fcace6862182d96df45bff73843d132
Palden D. Titung <paldendorjetitung@gmail.com>
2025-09-30 00:16:01 +0545

    add splash effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Products from "./Pages/Products";
@@ -10,7 +10,32 @@ import Cart from "./Pages/Cart";
 import ThankYouPage from "./Components/ThankYouPage";
 import ScrollToTop from "./Components/ScrollToTop";
 import PlaceHolderPage from "./Components/PlaceHolderPage";
+
 const App = () => {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowSplash(false), 2000); // 3 seconds
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (showSplash) {
+    return (
+      <div className=" min-h-screen flex justify-center items-center flex-col bg-white">
+        <div className="flex items-center gap-1">
+          {" "}
+          <span className="w-10 h-10 md:w-12 md:h-12 bg-blue-500 rounded-full flex justify-center items-center text-white font-bold italic text-lg md:text-xl">
+            BN
+          </span>
+          <span className="text-2xl md:text-3xl text-blue-500 font-bold">
+            BuyNext
+          </span>
+        </div>
+        <div className="w-[50px] h-[50px] border-blue-500 border-2  rounded-[50%] border-t-transparent  animate-spin"></div>
+      </div>
+    );
+  }
+
   return (
     <>
       <ScrollToTop />
